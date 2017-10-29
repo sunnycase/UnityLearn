50344191ccbdc3aad2d7165467594664fc79bc98
Guo Hui <sunnycase@live.cn>
2017-10-29 15:24:36 +0800

    complete Roll a Ball

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 _offset;
+    
+    private void Start()
+    {
+        _offset = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + _offset;
+    }
+}
--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    void FixedUpdate()
-    {
+    public float speed;
+    public Text countText;
+    public Text winText;
 
+    private Rigidbody _rigidbody;
+    private int _count;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _count = 0;
+        SetCountText();
+        winText.text = string.Empty;
+    }
+
+    private void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+        _rigidbody.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            _count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + _count;
+        if (_count >= 12)
+            winText.text = "You Win!";
     }
 }
